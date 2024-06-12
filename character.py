17cff386fc8067d9ee41a23fd65cbd271d125510
Nils <nils@trilobyte.de>
2024-06-12 19:34:34 +0200

    hab ein paar Game Loop Code Zeilen gelöscht und in main gepackt

--- character.py
+++ character.py
@@ -3,26 +3,25 @@ import pygame
 # Global variables
 COLOR = (167, 255, 100)
 SURFACE:COLOR = ( 0, 0, 0)
- WIDTH = 100
-  HEIGHT = 100
+WIDTH = 100
+HEIGHT = 100
 
   #OBJECT CLASS
-  class Sprite(pygame.sprite.Sprite):
-      def __init__(self, color, height, width):
-          super().__init__()
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, color, height, width):
+        super().__init__()
 
-          self.image =pygmae.Surface([width, height])
-          self.image.fill(SURFACE:COLOR)
-          self.image.set__colorkey(COLOR)
+        self.image =pygame.Surface([width, height])
+        self.image.fill(SURFACE:COLOR)
+        self.image.set__colorkey(COLOR)
 
-          pygame.draw.rect(self.image,
-                           color,
-                           pygmae.Rect(0, 0, width, height))
-          self.rect = self.image.get_rect()
-# Moving
+        pygame.draw.rect(self.image,
+                        color,
+                        pygmae.Rect(0, 0, width, height))
+        self.rect = self.image.get_rect()
+
     def moveRight (self, pixels):
         self.rect.x += pixels
-
     def moveLeft (self, pixels):
         self.rect.x += pixels
     def moveForward(self, pixels):
@@ -30,13 +29,6 @@ SURFACE:COLOR = ( 0, 0, 0)
     def moveBack(self, speed):
         self.rect.y -= speed * speed/10
 
-  pygame.init()
-
-  RED = (255, 0, 0)
-
-  size = (WIDTH, HEIGHT)
-  screen = pygame.display.set_mode(size)
-  pygmae.display.set_caption("Creating Sprite")
 
   all_sprites_list = pygame.sprite.Group()
 
@@ -46,16 +38,8 @@ SURFACE:COLOR = ( 0, 0, 0)
 
   all_sprites_list.add(object_)
 
-  exit = True
-  clock = pygame.time.Clock()
-
-  while exit:
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              exit = False
-          elif event.type == pygame.Keydown:
-              if event.key == pygame.K_x:
-                 exit = False
+  #
+ 
   #if keys get pressed do the following
       keys = pygame.key.get_pressed()
       if keys[pygame.K_LEFT]:
@@ -69,9 +53,4 @@ SURFACE:COLOR = ( 0, 0, 0)
 
   
       all_sprites_list.update()
-      screen.fill(SURFACE_COLOR)
-      all_sprites_list.draw(screen)
-      pygame.display.flip()
-      clock.tick(60)
-  pygame.quit()
 
