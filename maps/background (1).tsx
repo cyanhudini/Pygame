2a03ea174ce5be7ca154245eece832b5328584da
Binki1202 <s86004@bht-berlin.de>
2024-06-23 17:28:33 +0200

    push2

---
+++ maps/background (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="background (1)" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="background (1).jpg" width="1024" height="768"/>
+</tileset>
